7db146a728f0746bcf6eb6be64161a4a622c1866
Raditya Bagja Saputra <121217758+Radit1093@users.noreply.github.com>
2023-01-23 19:06:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
- struct BahanMakanan {
+struct BahanMakanan {
         int id;
         string nama;
         int jumlah;
@@ -10,77 +9,88 @@ using namespace std;
         string keterangan;
 };
 
-    BahanMakanan daftarBahan[5] = {
-        {
-            1,
-            "Tepung",
-            3,
-            12000,
-            "1 tepung = 1 kg ",
-        },
-        {
-            2,
-            "Telor",
-            90,
-            20000,
-            "16 telor = 1 kg"
-        },
-        {
-            3,
-            "Mentega",
-            3,
-            12000,
-            "1 mentega = 1 kg"
-        },
-        {
-            4,
-            "Gula",
-            3,
-            14500,
-            "1 gula = 1 kg"
-        },
-        {
-            5,
-            "Garam",
-            3,
-            12500,
-            "1 garam = 1 kg"
-        }
-    };
+BahanMakanan daftarBahan[5] = {
+    {
+        1,
+        "Tepung",
+        3,
+        12000,
+        "1 tepung = 1 kg ",
+    },
+    {
+        2,
+        "Telor",
+        90,
+        20000,
+        "16 telor = 1 kg"
+    },
+    {
+        3,
+        "Mentega",
+        3,
+        12000,
+        "1 mentega = 1 kg"
+    },
+    {
+        4,
+        "Gula",
+        3,
+        14500,
+        "1 gula = 1 kg"
+    },
+    {
+        5,
+        "Garam",
+        3,
+        12500,
+        "1 garam = 1 kg"
+    }
+};
 
-void prosesPembelian (const BahanMakanan bahan) {
-
+int menghitungUangKembalian(int uangBayar, int totalHarga) {
+    return uangBayar - totalHarga;
 }
 
 int main() {
-    string nama_pembeli;
-    int nomor_bahan, uang_bayar, jumlah_bahan;
-    cout << "=====DAFTAR BAHAN MAKANAN=====" << endl;
-   for (int i = 0; i < 5; i++) {
-    cout << i + 1 << ". " << daftarBahan[i].nama << "\n";
-   }
-    cout << "Pilih Bahan Makanan ?" << endl;
-    cin >> nomor_bahan;
+    char repeat;
+    do {
+        string nama_pembeli, bahan_makan;
+        int nomor_bahan, uang_bayar, jumlah_bahan;
+        
+        cout << "===== TOKO SERBA MAJU ===== \n" << endl;
+        for (int i = 0; i < 5; i++) {
+            cout << i + 1 << ". " << daftarBahan[i].nama << "\n";
+        }
 
-cout << "Masukkan Nama Pembeli" << endl;
-cin >> nama_pembeli;
-cout << "Pilih Nomor Bahan" << endl;
-cin >> nomor_bahan;
-cout << "Masukkan Jumlah Bahan" << endl;
-cin >> jumlah_bahan;
-cout << "Masukkan Jumlah Bayar" << endl;
-cin >> uang_bayar;
+        cout << "\n";
+        cout << "============================== \n" << endl;
+        cout << "\n";
+        cout << "Masukkan nama pembeli : ";
+        getline(cin, nama_pembeli);
+        if (nama_pembeli != "") {
+            cout << "\n";
+            cout << "Pilih nomor bahan : ";
+            cin >> nomor_bahan;
+            cout << "\n";
+            cout << "Rincian bahan : " << daftarBahan[nomor_bahan - 1].nama << " ~ Rp " << daftarBahan[nomor_bahan - 1].harga << " | " << daftarBahan[nomor_bahan - 1].keterangan  << "\n\n";
+            cout << "Masukkan jumlah bahan : ";
+            cin >> jumlah_bahan;
+            cout << "\n";
+            cout << "Total harga : Rp " << daftarBahan[nomor_bahan - 1].harga * jumlah_bahan << "\n";
+            cout << "\n";
+            cout << "Masukkan uang bayar : Rp ";
+            cin >> uang_bayar;
+            cout << "\n";
+            cout << "Uang kembalian : Rp " << menghitungUangKembalian(uang_bayar, (daftarBahan[nomor_bahan - 1].harga * jumlah_bahan)) << "\n";
+            cout << "\n";
+            cout << "Do you want to repeat purchasing? (y)es / (n)o : ";
+            cin >> repeat;
+            system ("CLS");
+        } else {
+            cout << "Mohon masukkan nama!";
+        }
 
-    if (nomor_bahan == 1){
-        cout << "Tepung per kg = 12000 jadi total kembalian Anda sebesar RP. " << uang_bayar-(12000*jumlah_bahan) << endl;
-    } else if (nomor_bahan == 2){
-        cout << "Telor per kg = 16 butir 20000 jadi total kembalian Anda sebesar RP. " << uang_bayar-(20000*jumlah_bahan) << endl;
-    } else if (nomor_bahan == 3){
-        cout << "Mentega per kg = 12000 jadi total kembalian Anda sebesar RP. " << uang_bayar-(12000*jumlah_bahan) << endl;
-    } else if (nomor_bahan == 4){
-        cout << "Gula per kg = 14500 jadi total kembalian Anda sebesar RP. " << uang_bayar-(14500*jumlah_bahan) << endl;
-    } else if (nomor_bahan == 5){
-        cout << "Garam per kg = 12500 jadi total kembalian Anda sebesar RP. " << uang_bayar-(12500*jumlah_bahan) << endl;
-    }
-
+        system ("CLS");
+        cout << "TERIMA KASIH TELAH BERBELANJA DISINI!, SILAHKAN DATANG KEMBALI!!" << endl;
+    } while (repeat == 'y');
 }
